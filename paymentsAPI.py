52d08621c274e14085b0f96d50955892b93e5fc8
tiagoadonis <tiago.adonis@ua.pt>
2023-04-06 16:08:16 +0100

    CORS requests

--- paymentsAPI.py
+++ paymentsAPI.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 from src import payments_v1, payments_v2, crud, models, schemas
 from sqlalchemy.orm import Session
-
 from src.database import SessionLocal, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 def create_app():
     app = FastAPI()
+
+    origins = [
+        "http://localhost:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     app.include_router(payments_v1.router, prefix="/v1")
     app.include_router(payments_v2.router, prefix="/v2")
